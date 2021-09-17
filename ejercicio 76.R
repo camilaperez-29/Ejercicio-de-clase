795dd859ee612e42aa08b10f253a417f12d66905
camilaperez-29 <maria.perez29@udea.edu.co>
2021-09-17 13:49:39 -0500

    edite script

--- ejercicio 76.R
+++ ejercicio 76.R
@@ -2,11 +2,16 @@
 
 serie <- function(x,t){
   resultado <- x/2 
-  for (i in 2:t){
-    sumatoria <- ((i) * ((x)^i)) / (i+1)
-    resultado <- resultado + sumatoria
+  for (i in 2:10000){
+    termino   <- ((i) * ((x)^i)) / (i+1)
+    resultado <- resultado + termino
+    ult_ter   <- abs(termino)
+    if (ult_ter <= t){
+      break
+    }
   }
    return(resultado)
 }
 
-serie(1,3)
+serie(0.5,0.01)
+
