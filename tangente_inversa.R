c8db0064f3306a3264c1921dd50f3a8d7e721335
camilaperez-29 <maria.perez29@udea.edu.co>
2021-09-17 12:17:14 -0500

    tangente inversa actualizada

--- tangente_inversa.R
+++ tangente_inversa.R
@@ -1,23 +1,56 @@
 
-''' 
- este codigo devuelve la funcion tangente inversa de valores entre -1 y 1.
+'''
+ este codigo devuelve valores aproximados de la funcion tangente inversa de valores entre -1 y 1.
    parametros de entrada:
      n:hasta que numero queremos que se resulva la sumatoria
      x:valor a evaluar
    parametros de salida:
-     arctan: valor aproximado de tangente inversa de x
+     resultado: valor aproximado de tangente inversa de x
+
 Autor : camila perez
 Ultima actualizacion : 15 de septiembre 2021
 
 '''
 
 arctan <- function (n,x){
-  sum = 0
+  resultado = 0
   for (i in 1:n){
-    suma = (-1)^(i+1) * ((x)^(2*i-1)) / (2*i-1)
-    sum = sum + suma
+    sumatoria = (-1)^(i+1) * ((x)^(2*i-1)) / (2*i-1)
+    resultado = resultado + sumatoria
   }
-  return(sum)
+  return(resultado)
 }
 
-arctan(10,1)
+
+
+#ejercicio a 
+
+pi = 4*arctan(1000,1)
+print(pi)
+
+
+#ejercicio b
+
+ 
+                         
+resultado = 0          #inicializa acomulador          
+i = 0                  #inicializa contador
+bandera = 'false'      #inicializa bandera
+
+while( resultado >= 0 & resultado <= 4 & bandera == 'false' ){           
+  i = i+1
+  sumatoria = (-1)^(i+1) * ((1)^(2*i-1)) / (2*i-1)
+  pi = 4 * sumatoria
+  resultado = resultado + pi
+  if(resultado > 3.1405926 & resultado < 3.1425926){
+    bandera = 'true'
+  }
+}
+sprintf("el numero de terminos de (n) es: %d ",i)
+
+
+
+
+
+
+
